62822820d2b60cf03e536ea4997fe24bbdb31ab1
PicciMario <mario.piccinelli@gmail.com>
2011-01-25 15:36:47 +0100

    updates

--- main.py
+++ main.py
@@ -5,6 +5,24 @@
 
  (C)opyright 2010 Mario Piccinelli <mario.piccinelli@gmail.com>
  Released under MIT licence
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
 
 '''
 
@@ -95,6 +113,7 @@ def buttonBoxPress(event):
 
 			os.remove(manifest_tempfile)		
 	
+	# SEARCH button
 	elif (event.widget['text'] == "Search"):
 		
 		global pattern
@@ -117,8 +136,6 @@ def buttonBoxPress(event):
 		
 		textarea.mark_set("current", searchindex)
 		textarea.yview(searchindex)
-		
-		return
 
 	return ""
 		
